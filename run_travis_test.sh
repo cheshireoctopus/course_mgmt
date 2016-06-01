de7aaf0ebcafaf84fac7a34b1e1dc61e2b3da7f3
Matthew Moisen <mmoisen@cisco.com>
2016-06-01 00:21:05 -0700

    trying opbeat.io

--- run_travis_test.sh
+++ run_travis_test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 python run.py > /dev/null &
 nosetests --with-coverage
+curl https://intake.opbeat.com/api/v1/organizations/f9912cfead99443481626ec8571446dc/apps/bf410a53b2/releases/ -H "Authorization: Bearer f76c0fc391048436e4bfc5dd8d7df07835adfe25" -d rev=`git log -n 1 --pretty=format:%H` -d branch=`git rev-parse --abbrev-ref HEAD` -d status=completed
