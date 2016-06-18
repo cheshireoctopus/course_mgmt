5e197be1f8cd3fb30d8c89c57b64663003b85185
Matthew Moisen <mmoisen@cisco.com>
2016-06-18 13:44:26 -0700

    heroku added port environment variable

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 from course_mgmt import app
+import os
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+port = int(os.environ.get('PORT', 5000))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
